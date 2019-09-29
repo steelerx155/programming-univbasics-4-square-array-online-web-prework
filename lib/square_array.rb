829ec95233acbd71503e34c7787805554cf969d8
Randy Steele <steelerx155@gmail.com>
2019-09-29 22:51:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,19 +1,8 @@
 def square_array(array)
-  counter = 0 
-  x = []
- while array[counter] do
-x << array[counter]**2
-    counter += 1
-  return x
-
-end 
-
-def square_array(array)
   counter = 0
   x = [] 
   while array[counter] do
     x << array[counter]**2
     counter += 1
+  end
   return x
-    
-end
